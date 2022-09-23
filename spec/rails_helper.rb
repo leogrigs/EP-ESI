18aa7a2a15097d81febd0ffc1b2016297de69fe6
leogrigs <leo.grigorio@usp.br>
2022-09-22 21:24:21 -0300

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,20 @@
 require 'coveralls'
 require 'simplecov'
 Coveralls.wear!('rails')
+SimpleCov.start 'rails' do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
