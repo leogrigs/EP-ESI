0f1e7c1f6f838b76868576d17bda572c38b02dde
Leonardo Grigorio Ferreira <leogrigs@MacBook-Air-de-LeoGrigs.local>
2022-10-10 15:03:54 -0300

    feat: card seed & more styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+    c = Card.new({name: "Card #{i}", description: "This is description for card #{i}", status: "todo"})
+    c.save
+end
