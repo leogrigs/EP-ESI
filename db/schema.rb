2ee0f8e7c9f21824f404e2d39a119a67827acc91
leogrigs <leo.grigorio@usp.br>
2022-10-11 21:04:53 -0300

    fix: coverage local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_153432) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_054159) do
   create_table "cards", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_10_153432) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "tests", force: :cascade do |t|
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_054159) do
   create_table "groups", force: :cascade do |t|
     t.string "description"
     t.string "card_qtd"
